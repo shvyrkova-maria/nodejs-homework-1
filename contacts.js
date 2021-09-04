38ce9cbecaa948772d62a39696cadbc816469d11
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-09-04 19:38:23 +0300

    Add contacts-cli

--- contacts.js
+++ contacts.js
@@ -1,9 +1,47 @@
-// const contactsPath = ;
+// const { nanoid } = require("nanoid");
+const fs = require("fs/promises");
+const path = require("path");
 
-// function listContacts() {}
+const contactsPath = path.join(__dirname, "/db/contacts.json");
 
-// function getContactById(contactId) {}
+async function listContacts() {
+  const contactsFile = await fs.readFile(contactsPath, "utf8");
+  const contactsParsed = JSON.parse(contactsFile);
+  console.table(contactsParsed);
+}
 
-// function removeContact(contactId) {}
+async function getContactById(contactId) {
+  const contactsFile = await fs.readFile(contactsPath, "utf8");
+  const contactsParsed = JSON.parse(contactsFile);
 
-// function addContact(name, email, phone) {}
+  contactsParsed.map((contact) => {
+    if (contact.id === contactId) {
+      console.log(contact);
+    }
+  });
+}
+
+async function removeContact(contactId) {
+  const contactsFile = await fs.readFile(contactsPath, "utf8");
+  const contactsParsed = JSON.parse(contactsFile);
+  const contactsFiltred = contactsParsed.filter(
+    ({ id }) => id !== Number(contactId)
+  );
+  await fs.writeFile(contactsPath, JSON.stringify(contactsFiltred, null, 2));
+  console.table(contactsFiltred);
+}
+
+async function addContact(name, email, phone) {
+  const contactsFile = await fs.readFile(contactsPath, "utf8");
+  const contactsParsed = JSON.parse(contactsFile);
+  contactsParsed.push({
+    // id: nanoid(2),
+    name: name,
+    email: email,
+    phone: phone,
+  });
+  await fs.writeFile(contactsPath, JSON.stringify(contactsParsed, null, 2));
+  console.table(contactsParsed);
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
